754a5fe50d453384f21aa431d7e7116dc8df912a
rbc33 <ricardobenthem@gmail.com>
2026-03-12 19:48:42 +0100

    cors

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const PORT = 3001
 app.use(express.json())
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://seoanalyst.es'],
     methods: ['GET', 'POST'],
   })
 )
